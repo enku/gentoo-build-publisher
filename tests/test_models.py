enku/gentoo-build-publisher

commit a4cc0df6fba1408308430d035812631b5faa8576

publish: don't remove previous build(s)
Instead moving the previous build to *.old and copying over it. We keep
each build as repos/<build> and binpkgs/<build> and then
create/overwrite a simlink to repos/<profile> and binpkgs/<profile>.

We'll later add a command to clean out old builds.

src/gentoo_build_publisher/models.py
@@ -2,7 +2,7 @@
 Django models for Gentoo Build Publisher
 """
 import os
-import uuid
+import shutil
 
 import requests
 from django.db import models
@@ -47,7 +47,7 @@ class Build(models.Model):
         return f"{name}(build_name={build_name!r}, build_number={build_number})"
 
     def __str__(self) -> str:
-        return f"Build #{self.build_number} for {self.build_name}"
+        return f"{self.build_name}.{self.build_number}"
 
     @property
     def url(self) -> str:
@@ -57,34 +57,43 @@ class Build(models.Model):
             f"/artifact/{settings.JENKINS_ARTIFACT_NAME}"
         )
 
-    def publish(self) -> uuid.UUID:
-        """Queue the publish task and return the task id"""
-        if not os.path.exists(self.artifact_path):
+    def publish(self):
+        """Make this build 'active'"""
+        repos_target = f"{settings.WORK_DIR}/repos/{self.build_name}"
+        binpkgs_target = f"{settings.WORK_DIR}/binpkgs/{self.build_name}"
+
+        if not os.path.exists(repos_target) and not os.path.exists(binpkgs_target):
             self.download_artifact()
 
-        tmpdir = f"{settings.WORK_DIR}/tmp/{self.build_name}.{self.build_number}"
-        io.extract_tarfile(self.artifact_path, tmpdir)
-        io.replace(f"{self.binpkgs_dir}/{self.build_name}", f"{tmpdir}/binpkgs")
-        io.replace(f"{self.repos_dir}/{self.build_name}", f"{tmpdir}/repos")
-        os.rmdir(tmpdir)
+        io.symlink(str(self), f"{settings.WORK_DIR}/repos/{self.build_name}")
+        io.symlink(str(self), f"{settings.WORK_DIR}/binpkgs/{self.build_name}")
 
     def download_artifact(self):
-        """Download the artifact from Jenkins to self.artifact_path"""
+        """Download the artifact from Jenkins
+
+        * extract repos to WORKDIR/repos/<build_name>.<build_number>
+        * extract binpkgs to WORKDIR/binpkgs/<build_name>.<build_number>
+        """
         auth = (settings.JENKINS_USER, settings.JENKINS_API_KEY)
         response = requests.get(self.url, auth=auth, stream=True)
         response.raise_for_status()
 
-        os.makedirs(os.path.dirname(self.artifact_path))
+        path = f"{settings.WORK_DIR}/tmp/{self}/build.tar.gz"
+        dirpath = os.path.dirname(path)
+        os.makedirs(dirpath, exist_ok=True)
 
-        with open(self.artifact_path, "wb") as artifact_file:
+        with open(path, "wb") as artifact_file:
             for chunk in response.iter_content(chunk_size=2048, decode_unicode=False):
                 artifact_file.write(chunk)
 
-    @property
-    def artifact_path(self):
-        """Return artifact path for this build"""
+        io.extract_tarfile(path, dirpath)
 
-        return (
-            f"{settings.WORK_DIR}/artifacts/{self.build_name}/{self.build_number}"
-            f"/{settings.JENKINS_ARTIFACT_NAME}"
-        )
+        source = f"{dirpath}/repos"
+        target = f"{settings.WORK_DIR}/repos/{self}"
+        os.rename(source, target)
+
+        source = f"{dirpath}/binpkgs"
+        target = f"{settings.WORK_DIR}/binpkgs/{self}"
+        os.rename(source, target)
+
+        shutil.rmtree(dirpath)

src/gentoo_build_publisher/io.py
@@ -1,26 +1,19 @@
 """GBP io"""
 import os
-import shutil
 import tarfile
 
 
 def extract_tarfile(name: str, target_dir: str):
     """Extract gzipped-tarball into work_dir"""
-    os.makedirs(target_dir)
-
     with tarfile.open(name, mode="r") as tar_file:
         tar_file.extractall(target_dir)
 
 
-def replace(old: str, new: str):
-    """Replace old with new, preserving old"""
-    if os.path.exists(old):
-        new_old_name = f"{old}.old"
+def symlink(source: str, target: str):
+    """If target is a symlink remove it. If it otherwise exists raise an error"""
+    if os.path.islink(target):
+        os.unlink(target)
+    elif os.path.exists(target):
+        raise EnvironmentError(f"{target} exists but is not a symlink")
 
-        if os.path.exists(new_old_name):
-            shutil.rmtree(new_old_name)
-
-        os.rename(old, new_old_name)
-
-    os.makedirs(os.path.dirname(old), exist_ok=True)
-    os.rename(new, old)
+    os.symlink(source, target)

commit b9a8db57395e3067321525ca9a081323539bc2a3

Build.publish: fix logic error
Bug was introduced by commit a4cc0df. It should be looking at the
extracted files and not the symlinks.

Also added the begginings of tests (this bug tested).

runtests.py
@@ -1,0 +1,18 @@
+#!/usr/bin/env python
+import os
+import sys
+
+import django
+from django.conf import settings
+from django.test.utils import get_runner
+
+
+if __name__ == "__main__":
+    os.environ["DJANGO_SETTINGS_MODULE"] = "tests.test_settings"
+    django.setup()
+
+    TestRunner = get_runner(settings)
+    test_runner = TestRunner()
+    failures = test_runner.run_tests(["tests"])
+
+    sys.exit(bool(failures))

src/gentoo_build_publisher/models.py
@@ -66,10 +66,7 @@ class Build(models.Model):
 
     def publish(self):
         """Make this build 'active'"""
-        repos_target = f"{settings.WORK_DIR}/repos/{self.build_name}"
-        binpkgs_target = f"{settings.WORK_DIR}/binpkgs/{self.build_name}"
-
-        if not os.path.exists(repos_target) and not os.path.exists(binpkgs_target):
+        if not os.path.exists(self.repos_dir) and not os.path.exists(self.binpkgs_dir):
             self.download_artifact()
 
         io.symlink(str(self), f"{settings.WORK_DIR}/repos/{self.build_name}")

tests/test_models.py
@@ -1,0 +1,72 @@
+"""Unit tests for gbp models"""
+import datetime
+import os
+import tempfile
+from unittest import mock
+
+from django.test import TestCase
+
+from gentoo_build_publisher.conf import settings
+from gentoo_build_publisher.models import Build
+
+
+class BuildTestCase(TestCase):
+    """Unit tests for the Build model"""
+
+    def setUp(self):
+        super().setUp()
+        self.temp_dir = tempfile.TemporaryDirectory()
+        self.addCleanup(self.temp_dir.cleanup)
+        self.work_dir = self.temp_dir.name
+        patch = mock.patch.object(settings, "WORK_DIR", self.work_dir)
+        patch.start()
+        self.addCleanup(patch.stop)
+
+        submitted = datetime.datetime(2021, 3, 23, 18, 39).replace(
+            tzinfo=datetime.timezone.utc
+        )
+        self.build = Build.objects.create(
+            build_name="babette", build_number=193, submitted=submitted
+        )
+
+    def test_publish_downloads_archive_if_repos_dir_does_not_exit(self):
+        """Should download the archive if repos/<name>.<number> doesn't exist"""
+        # Given the build
+        build = self.build
+
+        # When we call its publish method
+        with mock.patch.object(build, "download_artifact") as mock_download_artifact:
+            with mock.patch("gentoo_build_publisher.models.os.symlink") as mock_symlink:
+                build.publish()
+
+        # Then it downloads the artifact
+        mock_download_artifact.assert_called()
+
+        # And creates the symlinks
+        source = "babette.193"
+        mock_symlink.assert_any_call(source, f"{self.work_dir}/repos/babette")
+        mock_symlink.assert_any_call(source, f"{self.work_dir}/binpkgs/babette")
+
+    def test_downloads_archive_given_existing_symlinks(self):
+        """Bug"""
+        # Given the build
+        build = self.build
+
+        # given the existing symlinks
+        os.makedirs(f"{self.work_dir}/repos")
+        os.symlink(".", f"{self.work_dir}/repos/babette")
+        os.makedirs(f"{self.work_dir}/binpkgs")
+        os.symlink(".", f"{self.work_dir}/binpkgs/babette")
+
+        # When we call its publish method
+        with mock.patch.object(build, "download_artifact") as mock_download_artifact:
+            with mock.patch("gentoo_build_publisher.models.os.symlink") as mock_symlink:
+                build.publish()
+
+        # Then it downloads the artifact
+        mock_download_artifact.assert_called()
+
+        # And creates a symlink in the directory
+        source = "babette.193"
+        target = f"{self.work_dir}/repos/babette"
+        mock_symlink.assert_any_call(source, target)

tests/test_settings.py
@@ -1,0 +1,22 @@
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+SECRET_KEY = "test"
+
+USE_TZ = True
+
+BUILD_PUBLISHER = {
+    "JENKINS_BASE_URL": "http://jenkins.invalid/job/Gentoo",
+    "JENKINS_API_KEY": "jenkins-key",
+    "JENKINS_USER": "jenkins-user",
+}
+
+INSTALLED_APPS = ["django.contrib.contenttypes", "gentoo_build_publisher"]
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
+}