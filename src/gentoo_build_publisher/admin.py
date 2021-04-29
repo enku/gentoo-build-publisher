2abf0a231faa04fd4487f3e1ffd085f098709f19
Albert Hopkins <marduk@letterboxes.org>
2021-04-29 15:21:07 -0700

    add ModelAdmin for BuildModels
    This is primarily so we can flag builds for keeping.

---
+++ src/gentoo_build_publisher/admin.py
@@ -0,0 +1,23 @@
+"""Admin for Gentoo Build Publisher"""
+from django.contrib import admin
+
+from gentoo_build_publisher.models import BuildModel
+
+
+class BuildModelAdmin(admin.ModelAdmin):
+    """"ModelAdmin for the BuildModel"""
+
+    list_display = ["name", "number", "submitted", "completed", "keep"]
+    readonly_fields = ["name", "number", "submitted", "completed", "task_id"]
+
+    def has_add_permission(self, request):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        if obj is None:
+            return super().has_delete_permission(request, None)
+
+        return not obj.keep
+
+
+admin.site.register(BuildModel, BuildModelAdmin)
