9a4c0e08716c4b5ad90ce6ee3aadac7b3ff56da8
Albert Hopkins <marduk@letterboxes.org>
2021-06-05 10:39:28 -0700

    urls: add logs view to urlconf

--- src/gentoo_build_publisher/urls.py
+++ src/gentoo_build_publisher/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
     path("pull/<build_name>/<int:build_number>/", views.pull, name="pull"),
     path("latest/<build_name>/", views.latest, name="latest"),
     path("builds/<build_name>/", views.list_builds, name="list_builds"),
+    path("logs/<build_name>/<int:build_number>/", views.logs, name="logs"),
     path("admin/", admin.site.urls),
 ]
