89e723356dca6f2381b1646025df56a58cf75a36
Albert Hopkins <marduk@letterboxes.org>
2021-06-19 06:08:31 -0700

    Remove storage when model is deleted
    For situations like the Django admin or other django parts that are
    decoupled from the rest of the app, if the model gets deleted then the
    filesystem storage doesn't. This change adds a Django `post_delete`
    signal handler to ensure that everything gets cleaned up.

---
+++ src/gentoo_build_publisher/signals.py
@@ -0,0 +1,17 @@
+"""Signal handlers for Gentoo Build Publisher"""
+from django.db.models.signals import post_delete
+from django.dispatch import receiver
+
+from gentoo_build_publisher.build import Build
+from gentoo_build_publisher.managers import BuildMan
+from gentoo_build_publisher.models import BuildModel
+
+
+@receiver(post_delete, sender=BuildModel, dispatch_uid="buildmodel-deleted")
+def build_model_deleted(
+    sender, signal, instance, using
+):  # pylint: disable=unused-argument
+    """Signal handler for when a BuildModel is deleted"""
+    buildman = BuildMan(Build(name=instance.name, number=instance.number))
+
+    buildman.delete()
