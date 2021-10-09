7adb34c767f7ed63583113f89cfa2a8897e7ad34
Albert Hopkins <marduk@letterboxes.org>
2021-10-09 11:28:30 -0500

    views: remove REST API
    Everything has been moved over to the GraphQL API so the REST views are
    no longer needed.

--- src/gentoo_build_publisher/urls.py
+++ src/gentoo_build_publisher/urls.py
@@ -7,13 +7,6 @@ from gentoo_build_publisher import views
 from gentoo_build_publisher.graphql import schema
 
 urlpatterns = [
-    path("api/builds/<build_name>/", views.list_builds),
-    path("api/builds/<build_name>/<int:build_number>", views.api_build),
-    path("api/builds/<build_name>/<int:build_number>/log", views.logs),
-    path("api/builds/<build_name>/<int:build_number>/publish", views.publish),
-    path("api/builds/<build_name>/diff/<int:left>/<int:right>", views.diff_builds),
-    path("api/builds/<build_name>/latest", views.latest),
-    path("api/machines/", views.list_machines),
     path("", views.index),
     path("admin/", admin.site.urls),
     path("graphql", GraphQLView.as_view(schema=schema), name="graphql"),
