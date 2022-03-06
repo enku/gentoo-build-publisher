21b25994fce44ad9a58da82c08084f389a323792
Albert Hopkins <marduk@letterboxes.org>
2022-03-06 06:01:39 -0600

    add management command to do a system check
    Check the filesystem for orphans (directories without records) and check
    the database of builds to ensure each build has all the required
    directories.

---
+++ src/gentoo_build_publisher/management/commands/gbpchk.py
@@ -0,0 +1,48 @@
+"""Check GBP storage/db"""
+from django.core.management.base import BaseCommand, CommandError
+
+from gentoo_build_publisher.publisher import get_publisher
+from gentoo_build_publisher.records import RecordNotFound
+from gentoo_build_publisher.types import Build, Content
+
+
+class Command(BaseCommand):
+    """Command class for the gbpchk management command"""
+
+    help = "Check GBP storage and records"
+
+    def handle(self, *args, **options):
+        errors = 0
+        publisher = get_publisher()
+
+        # Pass 1: check build content
+        records = publisher.records.query(completed__isnull=False)
+
+        for record in records:
+            missing = []
+            for content in Content:
+                path = publisher.storage.get_path(record, content)
+
+                if not path.exists():
+                    missing.append(path)
+
+            if missing:
+                self.stderr.write(f"Path missing for {record}: {missing}")
+                errors += 1
+
+        # Pass 2: check for orphans
+        for content in Content:
+            directory = publisher.storage.path / content.value
+
+            for path in directory.glob("*.*"):
+                build = Build(path.name)
+
+                try:
+                    publisher.records.get(build)
+                except RecordNotFound:
+                    self.stderr.write(f"Record missing for {path}")
+                    errors += 1
+
+        if errors:
+            self.stderr.write("Errors were encountered.")
+            raise CommandError(errors)
