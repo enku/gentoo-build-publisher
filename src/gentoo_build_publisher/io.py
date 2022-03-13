9b71b8dfd155a04c2cf6227dffacff62c1c97939
Albert Hopkins <marduk@letterboxes.org>
2022-03-13 09:09:56 -0500

    io: using typing.IO instead of custom Protocols
    I'm learning to use typing.IO types instead of hacking my own.
    Pyright also likes this.

--- src/gentoo_build_publisher/io.py
+++ src/gentoo_build_publisher/io.py
@@ -2,30 +2,16 @@
 from __future__ import annotations
 
 from collections.abc import Iterator
-from typing import Protocol
+from typing import IO
 
 
-class Readable(Protocol):  # pylint: disable=too-few-public-methods
-    """File-like object that can read in chunks"""
-
-    def read(self, size: int) -> bytes:
-        """Read at most size bytes from the file"""
-
-
-class Writable(Protocol):  # pylint: disable=too-few-public-methods
-    """File-like object that can write bytes"""
-
-    def write(self, data: bytes) -> int:
-        """Write bytes and return the number of bytes written"""
-
-
-def read_chunks(reader: Readable, chunk_size: int) -> Iterator[bytes]:
+def read_chunks(reader: IO[bytes], chunk_size: int) -> Iterator[bytes]:
     """Iterate chunk_size-chunks over the file-like object"""
     while chunk := reader.read(chunk_size):
         yield chunk
 
 
-def write_chunks(writer: Writable, chunked_iter: Iterator[bytes]) -> int:
+def write_chunks(writer: IO[bytes], chunked_iter: Iterator[bytes]) -> int:
     """Given the chunked_iter iterable, write all chunks to writer"""
     bytes_written = 0
 
