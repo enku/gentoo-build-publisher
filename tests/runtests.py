117c1ade18f3f0dd423cd2096c366cccdd80ea13
Albert Hopkins <marduk@letterboxes.org>
2022-09-04 08:51:05 -0500

    tests: runtests.py: extract logic into a function

--- tests/runtests.py
+++ tests/runtests.py
@@ -7,14 +7,20 @@ import django
 from django.conf import settings
 from django.test.utils import get_runner
 
-if __name__ == "__main__":
+
+def main() -> None:
+    """Program entry point"""
     os.environ["DJANGO_SETTINGS_MODULE"] = "settings"
     django.setup()
 
     tests = sys.argv[1:] or ["."]
 
-    TestRunner = get_runner(settings)
+    TestRunner = get_runner(settings)  # pylint: disable=invalid-name
     test_runner = TestRunner(failfast=True, verbosity=2)
     failures = test_runner.run_tests(tests)
 
     sys.exit(bool(failures))
+
+
+if __name__ == "__main__":
+    main()
