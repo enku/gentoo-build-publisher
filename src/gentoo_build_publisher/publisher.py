82368d140f54628cff8d906d336cc443e101e7d0
Albert Hopkins <marduk@letterboxes.org>
2022-10-04 17:19:20 -0500

    publisher: gbp_metadata: only iterate over package list once

--- src/gentoo_build_publisher/publisher.py
+++ src/gentoo_build_publisher/publisher.py
@@ -236,18 +236,20 @@ class BuildPublisher:
         jenkins_metadata: JenkinsMetadata, packages: list[Package]
     ) -> GBPMetadata:
         """Generate GBPMetadata given JenkinsMetadata and Packages"""
+        built: list[Package] = []
+        jenkins_built_time = math.floor(jenkins_metadata.timestamp / 1000)
+        size = 0
+        total = 0
+
+        for package in packages:
+            if package.build_time >= jenkins_built_time:
+                built.append(package)
+            size += package.size
+            total += 1
+
         return GBPMetadata(
             build_duration=jenkins_metadata.duration,
-            packages=PackageMetadata(
-                total=len(packages),
-                size=sum(package.size for package in packages),
-                built=[
-                    package
-                    for package in packages
-                    if package.build_time
-                    >= math.floor(jenkins_metadata.timestamp / 1000)
-                ],
-            ),
+            packages=PackageMetadata(total=total, size=size, built=built),
         )
 
 
