ecfe4e8cff9b782cea831aad8fb666532632de7f
Albert Hopkins <marduk@letterboxes.org>
2024-01-05 06:09:41 -0600

    tests.test_templatetags: refactor
    Create a template that uses the tags/filter and render that instead of
    calling the tag/filter directly.

--- tests/test_templatetags.py
+++ tests/test_templatetags.py
@@ -1,65 +1,58 @@
 """Tests for custom template tags"""
 # pylint: disable=missing-class-docstring,missing-function-docstring
+from typing import Any
 from unittest import TestCase
 
-from django.template import TemplateSyntaxError
-from django.template.loader import get_template
+from django.template import Context, Template, TemplateSyntaxError
 
 from gentoo_build_publisher.common import Build
-from gentoo_build_publisher.templatetags.gbp import (
-    build_row,
-    chart,
-    circle,
-    numberize,
-    package_row,
-)
 
 
-class NumberizeTestCase(TestCase):
+class TemplateTagTests(TestCase):
+    library = "gbp"
+    template = "override me!"
+
+    def render(self, template: str | None = None, **kwargs: Any) -> str:
+        template = self.template if template is None else template
+        context = Context(kwargs)
+        return Template(f"{{% load {self.library} %}}{template}").render(context)
+
+
+class NumberizeTestCase(TemplateTagTests):
+    template = "{{ number|numberize:precision }}"
+
     def test_should_return_whole_number_when_precision_0(self) -> None:
-        value = 96_858_412
-
-        result = numberize(value, 0)
+        result = self.render(number=96_858_412, precision=0)
 
         self.assertEqual(result, "96M")
 
     def test_number_less_than_1000(self) -> None:
-        value = 968
-
-        result = numberize(value, 2)
+        result = self.render(number=968, precision=2)
 
         self.assertEqual(result, "968")
 
     def test_number_less_than_1_000_000(self) -> None:
-        value = 968_584
-
-        result = numberize(value, 1)
+        result = self.render(number=968_584, precision=1)
 
         self.assertEqual(result, "968.5k")
 
     def test_number_less_than_1_000_000_000(self) -> None:
-        value = 96_858_412
-
-        result = numberize(value, 2)
+        result = self.render(number=96_858_412, precision=2)
 
         self.assertEqual(result, "96.85M")
 
     def test_number_greater_than_1_000_000_000(self) -> None:
-        value = 6_123_000_548
-
-        result = numberize(value, 2)
+        result = self.render(number=6_123_000_548, precision=2)
 
         self.assertEqual(result, "6.12G")
 
     def test_invalid_value(self) -> None:
-        value = "this is not a number"
-
         with self.assertRaises(TemplateSyntaxError):
-            numberize(value, 2)
+            self.render(number="this is not a number", precision=2)
 
 
-class NumberedCircleTests(TestCase):
-    maxDiff = None
+class NumberedCircleTests(TemplateTagTests):
+    template = "{% circle build_count name color %}"
 
     def test(self) -> None:
         expected = """\
@@ -68,9 +61,7 @@ class NumberedCircleTests(TestCase):
   <h2>Builds</h2>
 </div>
 """
-        context = circle(452, "Builds", "#755245")
-        template = get_template("gentoo_build_publisher/circle.html")
-        result = template.render(context)
+        result = self.render(build_count=452, name="Builds", color="#755245")
         self.assertEqual(result, expected)
 
     def test_large_number(self) -> None:
@@ -80,13 +71,13 @@ class NumberedCircleTests(TestCase):
   <h2>Packages</h2>
 </div>
 """
-        context = circle(212351, "Packages", "#755245")
-        template = get_template("gentoo_build_publisher/circle.html")
-        result = template.render(context)
+        result = self.render(build_count=212351, name="Packages", color="#755245")
         self.assertEqual(result, expected)
 
 
-class ChartTests(TestCase):
+class ChartTests(TemplateTagTests):
+    template = "{% chart dom_id title cols=cols width=width height=height %}"
+
     def test(self) -> None:
         expected = """\
 <div class="col-md-8">
@@ -94,13 +85,15 @@ class ChartTests(TestCase):
   <canvas id="testChart" width="100" height="150"></canvas>
 </div>
 """
-        context = chart("testChart", "Test Test", cols=8, width=100, height=150)
-        template = get_template("gentoo_build_publisher/chart.html")
-        result = template.render(context)
+        result = self.render(
+            dom_id="testChart", title="Test Test", cols=8, width=100, height=150
+        )
         self.assertEqual(result, expected)
 
 
-class BuildRowTests(TestCase):
+class BuildRowTests(TemplateTagTests):
+    template = "{% build_row build build_packages %}"
+
     def test(self) -> None:
         expected = """\
 <li class="list-group-item d-flex justify-content-between lh-condensed">
@@ -114,13 +107,13 @@ class BuildRowTests(TestCase):
         build = Build("babette", "1094")
         packages = ["x11-libs/libdrm-2.4.118", "x11-misc/xkeyboard-config-2.40-r1"]
         build_packages = {"babette.1094": packages}
-        context = build_row(build, build_packages)
-        template = get_template("gentoo_build_publisher/build_row.html")
-        result = template.render(context)
+        result = self.render(build=build, build_packages=build_packages)
         self.assertEqual(result, expected)
 
 
-class PackageRowTests(TestCase):
+class PackageRowTests(TemplateTagTests):
+    template = "{% package_row package machines %}"
+
     def test(self) -> None:
         expected = """\
 <li class="list-group-item d-flex justify-content-between lh-condensed">
@@ -132,9 +125,7 @@ class PackageRowTests(TestCase):
 """
         package = "x11-libs/libdrm-2.4.118"
         machines = ["babette", "polaris"]
-        context = package_row(package, machines)
-        template = get_template("gentoo_build_publisher/package_row.html")
-        result = template.render(context)
+        result = self.render(package=package, machines=machines)
         self.assertEqual(result, expected)
 
     def test_single_machine(self) -> None:
@@ -148,7 +139,5 @@ class PackageRowTests(TestCase):
 """
         package = "x11-libs/libdrm-2.4.118"
         machines = ["babette"]
-        context = package_row(package, machines)
-        template = get_template("gentoo_build_publisher/package_row.html")
-        result = template.render(context)
+        result = self.render(package=package, machines=machines)
         self.assertEqual(result, expected)
