5bd0f11f368a21ffadeb23863c833adc72343e5c
Albert Hopkins <marduk@letterboxes.org>
2024-01-10 18:11:36 -0600

    add more time helper functions

--- src/gentoo_build_publisher/utils/time.py
+++ src/gentoo_build_publisher/utils/time.py
@@ -13,3 +13,30 @@ def utctime(time: dt.datetime | None = None) -> dt.datetime:
         time = dt.datetime.utcnow()
 
     return time.replace(tzinfo=dt.timezone.utc)
+
+
+def is_same_day(first: dt.datetime, second: dt.datetime) -> bool:
+    """Return True if dates are on the same day"""
+    return first.date() == second.date()
+
+
+def is_previous_day(first: dt.datetime, second: dt.datetime) -> bool:
+    """Return True if first is on the day before second"""
+    day_before = (second - dt.timedelta(days=1)).date()
+
+    return first.date() == day_before
+
+
+def as_time(time: dt.datetime) -> str:
+    """Return the time portion in the locale's format"""
+    return time.strftime("%H:%M:%S")
+
+
+def as_date(time: dt.datetime) -> str:
+    """Return the date portion in the locale's format"""
+    return time.strftime("%b %-d")
+
+
+def as_date_and_time(time: dt.datetime) -> str:
+    """Return the date and time in the locale's format"""
+    return time.strftime("%b %-d %H:%M")
--- tests/test_utils_time.py
+++ tests/test_utils_time.py
@@ -1,12 +1,19 @@
+"""Tests for the gentoo_build_publisher.utils module"""
 # pylint: disable=missing-docstring
 import datetime as dt
 from unittest import TestCase, mock
+from zoneinfo import ZoneInfo
 
 from gentoo_build_publisher.utils import time
 
+CT = ZoneInfo("America/Chicago")
+NOW = dt.datetime(2024, 1, 9, 21, 5, 5, tzinfo=CT)
+YESTERDAY = dt.datetime(2024, 1, 8, 9, 0, 0, tzinfo=CT)
+THREE_HOURS = dt.timedelta(hours=3)
+
 
 class UtcTime(TestCase):
-    """Tests for utils.utctime"""
+    """Tests for time.utctime"""
 
     def test_should_give_the_time_with_utc_timezone(self) -> None:
         timestamp = dt.datetime(2022, 9, 17, 17, 36)
@@ -32,3 +39,41 @@ class LapsedTestCase(TestCase):
         lapsed = time.lapsed(start, end)
 
         self.assertEqual(lapsed, 3661)
+
+
+class IsSameDayTests(TestCase):
+    def test_true(self) -> None:
+        first = NOW
+        second = NOW - THREE_HOURS
+
+        self.assertTrue(time.is_same_day(first, second))
+
+    def test_false(self) -> None:
+        first = NOW
+        second = NOW + THREE_HOURS
+
+        self.assertFalse(time.is_same_day(first, second))
+
+
+class IsPreviousDayTests(TestCase):
+    def test_true(self) -> None:
+        self.assertTrue(time.is_previous_day(YESTERDAY, NOW))
+
+    def test_false(self) -> None:
+        self.assertFalse(time.is_previous_day(NOW, YESTERDAY))
+
+
+class AsTimeTests(TestCase):
+    def test(self) -> None:
+        self.assertEqual(time.as_time(NOW), "21:05:05")
+        self.assertEqual(time.as_time(NOW), "21:05:05")
+
+
+class AsDateTests(TestCase):
+    def test(self) -> None:
+        self.assertEqual(time.as_date(NOW), "Jan 9")
+
+
+class AsDateAndTimeTests(TestCase):
+    def test(self) -> None:
+        self.assertEqual(time.as_date_and_time(NOW), "Jan 9 21:05")
