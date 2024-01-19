enku/gentoo-build-publisher

commit b30c6c9e92bd87ffa8e03e080e90ea51b5b2dd3f

views: fix builds over time chart
A bug was introduced when switching to the StatsCollector where the
builds over time data was not being converted to the local timezone.
This change fixes that and in addition does some refactoring to
utils.time.

src/gentoo_build_publisher/utils/time.py
@@ -1,16 +1,28 @@
 """Utilities for dealing with time and dates"""
 import datetime as dt
 
+LOCAL_TIMEZONE = dt.datetime.now().astimezone().tzinfo
+
+now = dt.datetime.now
+
 
 def lapsed(start: dt.datetime, end: dt.datetime) -> int:
     """Return the number of seconds between `start` and `end`"""
     return int((end - start).total_seconds())
 
 
+def localtime(time: dt.datetime | None = None) -> dt.datetime:
+    """Return the given time (or now) in the local timezone"""
+    if time is None:
+        time = now()
+
+    return time.astimezone(LOCAL_TIMEZONE)
+
+
 def utctime(time: dt.datetime | None = None) -> dt.datetime:
     """Return time but with the timezone being UTC"""
     if time is None:
-        time = dt.datetime.utcnow()
+        time = now()
 
     return time.replace(tzinfo=dt.timezone.utc)
 

tests/test_utils_time.py
@@ -22,9 +22,9 @@ class UtcTime(TestCase):
 
         self.assertEqual(result, timestamp.replace(tzinfo=dt.timezone.utc))
 
-    @mock.patch("gentoo_build_publisher.utils.time.dt.datetime")
-    def test_time_defaults_to_now(self, datetime: mock.Mock) -> None:
-        datetime.utcnow.return_value = utcnow = dt.datetime(2022, 9, 17, 17, 36)
+    @mock.patch("gentoo_build_publisher.utils.time.now")
+    def test_time_defaults_to_now(self, now: mock.Mock) -> None:
+        now.return_value = utcnow = dt.datetime(2022, 9, 17, 17, 36)
 
         result = time.utctime()
 

src/gentoo_build_publisher/templatetags/gbp.py
@@ -12,7 +12,7 @@ from gentoo_build_publisher.publisher import BuildPublisher
 from gentoo_build_publisher.settings import Settings
 from gentoo_build_publisher.utils import time
 
-dt_now = dt.datetime.now
+localtime = time.localtime
 register = template.Library()
 
 
@@ -49,8 +49,8 @@ def numberize(val: int, precision: int = 2) -> str:
 @register.filter
 def display_time(timestamp: dt.datetime) -> str:
     """Display the timestamp according to how long ago it was"""
-    timestamp = timestamp.astimezone()
-    now = dt_now().astimezone()
+    timestamp = localtime(timestamp)
+    now = localtime()
 
     if time.is_same_day(timestamp, now):
         return time.as_time(timestamp)
@@ -148,5 +148,5 @@ def machine_package_row(package: Package) -> dict[str, Any]:
 
     return {
         "package": package,
-        "build_time": dt.datetime.fromtimestamp(package.build_time).astimezone(),
+        "build_time": localtime(dt.datetime.fromtimestamp(package.build_time))
     }

src/gentoo_build_publisher/utils/views.py
@@ -13,7 +13,7 @@ from gentoo_build_publisher.common import Build, CacheProtocol, GBPMetadata, Pac
 from gentoo_build_publisher.publisher import BuildPublisher, MachineInfo
 from gentoo_build_publisher.records import BuildRecord
 from gentoo_build_publisher.utils import Color
-from gentoo_build_publisher.utils.time import lapsed
+from gentoo_build_publisher.utils.time import lapsed, localtime
 
 BuildID: TypeAlias = str  # pylint: disable=invalid-name
 CPV: TypeAlias = str  # pylint: disable=invalid-name
@@ -139,7 +139,7 @@ class StatsCollector:
         for build in mi.builds:
             if not build.submitted:
                 continue
-            date = build.submitted.date()
+            date = localtime(build.submitted).date()
             bbd[date] = bbd.setdefault(date, 0) + 1
 
         return bbd

tests/test_templatetags.py
@@ -7,6 +7,9 @@ from unittest import TestCase, mock
 from django.template import Context, Template, TemplateSyntaxError
 
 from gentoo_build_publisher.common import Build
+from gentoo_build_publisher.utils.time import localtime
+
+NOW = "gentoo_build_publisher.utils.time.now"
 
 
 class TemplateTagTests(TestCase):
@@ -169,24 +172,21 @@ class MachineLinkTests(TemplateTagTests):
         self.assertEqual(self.render("{{ 'lighthouse'|machine_link }}"), expected)
 
 
-@mock.patch(
-    "gentoo_build_publisher.templatetags.gbp.dt_now",
-    mock.Mock(return_value=dt.datetime(2024, 1, 11, 20, 54)),
-)
+@mock.patch(NOW, mock.Mock(return_value=dt.datetime(2024, 1, 11, 20, 54)))
 class DisplayTimeTests(TemplateTagTests):
     template = "{{ timestamp|display_time }}"
 
     def test_same_day(self) -> None:
-        timestamp = dt.datetime(2024, 1, 11, 8, 54).astimezone()
+        timestamp = localtime(dt.datetime(2024, 1, 11, 8, 54))
 
         self.assertEqual(self.render(timestamp=timestamp), "08:54:00")
 
     def test_previous_day(self) -> None:
-        timestamp = dt.datetime(2024, 1, 10, 8, 54).astimezone()
+        timestamp = localtime(dt.datetime(2024, 1, 10, 8, 54))
 
         self.assertEqual(self.render(timestamp=timestamp), "Jan 10 08:54")
 
     def test_previous_week(self) -> None:
-        timestamp = dt.datetime(2024, 1, 4, 8, 54).astimezone()
+        timestamp = localtime(dt.datetime(2024, 1, 4, 8, 54))
 
         self.assertEqual(self.render(timestamp=timestamp), "Jan 4")

tests/test_views_utils.py
@@ -2,11 +2,13 @@
 # pylint: disable=missing-docstring
 import datetime as dt
 from unittest import mock
+from zoneinfo import ZoneInfo
 
 from django.utils import timezone
 
 from gentoo_build_publisher.common import Build
 from gentoo_build_publisher.utils import Color
+from gentoo_build_publisher.utils.time import localtime
 from gentoo_build_publisher.utils.views import (
     StatsCollector,
     ViewInputContext,
@@ -128,7 +130,7 @@ class CreateDashboardContext(TestCase):
         self.assertEqual(cxt["unpublished_builds_count"], 2)
 
     def test_builds_over_time_and_build_recently(self) -> None:
-        now = timezone.localtime()
+        now = dt.datetime(2024, 1, 17, 4, 51, tzinfo=dt.timezone.utc)
         for machine in ["babette", "lighthouse"]:
             for day in range(2):
                 for _ in range(3):
@@ -141,7 +143,9 @@ class CreateDashboardContext(TestCase):
                     if day == 0:
                         break
 
-        cxt = create_dashboard_context(self.input_context())
+        localtimezone = "gentoo_build_publisher.utils.time.LOCAL_TIMEZONE"
+        with mock.patch(localtimezone, new=ZoneInfo("America/Chicago")):
+            cxt = create_dashboard_context(self.input_context())
         self.assertEqual(cxt["builds_over_time"], [[3, 1], [3, 1]])
         self.assertEqual(len(cxt["built_recently"]), 2)
 
@@ -282,19 +286,19 @@ class StatsCollectorTests(TestCase):
         for hour in range(2):
             self.publisher.record(BuildFactory(machine="babette")).save(
                 self.publisher.records,
-                submitted=dt.datetime(2024, 1, 13, 12).astimezone()
+                submitted=localtime(dt.datetime(2024, 1, 13, 12))
                 + dt.timedelta(hours=hour),
             )
         for hour in range(3):
             self.publisher.record(BuildFactory(machine="babette")).save(
                 self.publisher.records,
-                submitted=dt.datetime(2024, 1, 14, 12).astimezone()
+                submitted=localtime(dt.datetime(2024, 1, 14, 12))
                 + dt.timedelta(hours=hour),
             )
         for hour in range(4):
             self.publisher.record(BuildFactory(machine="babette")).save(
                 self.publisher.records,
-                submitted=dt.datetime(2024, 1, 15, 12).astimezone()
+                submitted=localtime(dt.datetime(2024, 1, 15, 12))
                 + dt.timedelta(hours=hour),
             )
 

commit 013e9038d608d3ff6657752da90f5009248ebe1a

fix timestamp (timezone) in submitted and completed fields
This change fixes a regression in commit b30c6c9 that's causing the
submitted and completed fields in BuildRecords to have the incorrect
times.

src/gentoo_build_publisher/utils/time.py
@@ -23,7 +23,7 @@ def localtime(time: dt.datetime | None = None) -> dt.datetime:
 def utctime(time: dt.datetime | None = None) -> dt.datetime:
     """Return time but with the timezone being UTC"""
     if time is None:
-        time = now()
+        return now(dt.timezone.utc)
 
     return time.replace(tzinfo=dt.timezone.utc)
 

tests/test_publisher.py
@@ -1,9 +1,10 @@
 """Tests for the GBP publisher"""
 # pylint: disable=missing-class-docstring,missing-function-docstring
-import datetime
+import datetime as dt
 import os
 import unittest
 from unittest import mock
+from zoneinfo import ZoneInfo
 
 from yarl import URL
 
@@ -17,7 +18,7 @@ from gentoo_build_publisher.utils.time import utctime
 from . import BUILD_LOGS, TestCase, set_up_tmpdir_for_test
 from .factories import BuildFactory
 
-utc = datetime.timezone.utc
+utc = dt.timezone.utc
 
 
 class BuildPublisherFromSettingsTestCase(unittest.TestCase):
@@ -86,7 +87,7 @@ class BuildPublisherTestCase(TestCase):
 
         record = self.publisher.record(self.build)
 
-        jenkins_timestamp = datetime.datetime.utcfromtimestamp(
+        jenkins_timestamp = dt.datetime.utcfromtimestamp(
             self.artifact_builder.build_info(self.build).build_time / 1000
         ).replace(tzinfo=utc)
         self.assertEqual(record.built, jenkins_timestamp)
@@ -109,6 +110,26 @@ class BuildPublisherTestCase(TestCase):
 
         self.assertFalse(self.publisher.pulled(self.build))
 
+    def test_build_timestamps(self) -> None:
+        localtimezone = "gentoo_build_publisher.utils.time.LOCAL_TIMEZONE"
+        with mock.patch(localtimezone, new=ZoneInfo("America/New_York")):
+            submitted = dt.datetime(2024, 1, 19, 11, 5, 49, tzinfo=dt.timezone.utc)
+            now = "gentoo_build_publisher.utils.time.now"
+            with mock.patch(now, return_value=submitted):
+                self.artifact_builder.timer = 1705662194  # 2024-01-19 11:03 UTC
+                build = BuildFactory()
+                self.publisher.pull(build)
+                record = self.publisher.record(build)
+
+        ct = ZoneInfo("America/Chicago")
+        self.assertEqual(record.built, dt.datetime(2024, 1, 19, 5, 3, 24, tzinfo=ct))
+        self.assertEqual(
+            record.submitted, dt.datetime(2024, 1, 19, 5, 5, 49, tzinfo=ct)
+        )
+        self.assertEqual(
+            record.completed, dt.datetime(2024, 1, 19, 5, 5, 49, tzinfo=ct)
+        )
+
     def test_pull_with_note(self) -> None:
         self.publisher.pull(self.build, note="This is a test")
 
@@ -130,14 +151,14 @@ class BuildPublisherTestCase(TestCase):
         self.publisher.pull(old_build)
         record = self.publisher.record(old_build)
         self.publisher.records.save(
-            record, submitted=datetime.datetime(1970, 1, 1, tzinfo=utc)
+            record, submitted=dt.datetime(1970, 1, 1, tzinfo=utc)
         )
 
         new_build = BuildFactory()
         self.publisher.pull(new_build)
         record = self.publisher.record(new_build)
         self.publisher.records.save(
-            record, submitted=datetime.datetime(1970, 12, 31, tzinfo=utc)
+            record, submitted=dt.datetime(1970, 12, 31, tzinfo=utc)
         )
 
         self.publisher.purge(old_build.machine)
@@ -154,19 +175,19 @@ class BuildPublisherTestCase(TestCase):
         kept_build = BuildFactory(machine="lighthouse")
         self.publisher.records.save(
             self.publisher.record(kept_build),
-            submitted=datetime.datetime(1970, 1, 1, tzinfo=utc),
+            submitted=dt.datetime(1970, 1, 1, tzinfo=utc),
             keep=True,
         )
         tagged_build = BuildFactory(machine="lighthouse")
         self.publisher.records.save(
             self.publisher.record(tagged_build),
-            submitted=datetime.datetime(1970, 1, 1, tzinfo=utc),
+            submitted=dt.datetime(1970, 1, 1, tzinfo=utc),
         )
         self.publisher.pull(tagged_build)
         self.publisher.tag(tagged_build, "prod")
         self.publisher.records.save(
             self.publisher.record(BuildFactory(machine="lighthouse")),
-            submitted=datetime.datetime(1970, 12, 31, tzinfo=utc),
+            submitted=dt.datetime(1970, 12, 31, tzinfo=utc),
         )
 
         self.publisher.purge("lighthouse")
@@ -179,11 +200,11 @@ class BuildPublisherTestCase(TestCase):
         self.publisher.publish(self.build)
         self.publisher.records.save(
             self.publisher.record(self.build),
-            submitted=datetime.datetime(1970, 1, 1, tzinfo=utc),
+            submitted=dt.datetime(1970, 1, 1, tzinfo=utc),
         )
         self.publisher.records.save(
             self.publisher.record(BuildFactory()),
-            submitted=datetime.datetime(1970, 12, 31, tzinfo=utc),
+            submitted=dt.datetime(1970, 12, 31, tzinfo=utc),
         )
 
         self.publisher.purge(self.build.machine)

tests/test_utils_time.py
@@ -24,7 +24,9 @@ class UtcTime(TestCase):
 
     @mock.patch("gentoo_build_publisher.utils.time.now")
     def test_time_defaults_to_now(self, now: mock.Mock) -> None:
-        now.return_value = utcnow = dt.datetime(2022, 9, 17, 17, 36)
+        now.return_value = utcnow = dt.datetime(
+            2022, 9, 17, 17, 36, tzinfo=dt.timezone.utc
+        )
 
         result = time.utctime()
 