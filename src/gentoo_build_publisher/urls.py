9f003f50506b65201d93d769043d2e39463e8eda
Albert Hopkins <marduk@letterboxes.org>
2025-02-07 22:45:54 -0600

    urls: automatically add registered apps' urls
    Automatically add registered app's urls (from app.urls.urlpatterns) to
    GBP's urlconf when they are defined.
    
    Making adding plugins easier (although presently no plugins have their
    own urls).

--- src/gentoo_build_publisher/urls.py
+++ src/gentoo_build_publisher/urls.py
@@ -1,5 +1,20 @@
 """Default urlconf for gentoo_build_publisher"""
 
+from importlib import import_module
+from importlib.metadata import entry_points
+
 from gentoo_build_publisher.views.utils import ViewFinder
 
 urlpatterns = ViewFinder.find()
+
+for entry_point in entry_points().select(group="gentoo_build_publisher.apps"):
+    app = entry_point.load()
+    try:
+        module = import_module(f"{app}.urls")
+    except ImportError:
+        pass
+    else:
+        urlpatterns.extend(getattr(module, "urlpatterns", []))
+        del module
+
+    del entry_point, app
