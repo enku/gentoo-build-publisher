3a8ac540734673dcc27282ef3e447bb697264e7d
Albert Hopkins <marduk@letterboxes.org>
2025-02-16 16:15:48 -0600

    utils: add dataclass deserialization helpers
    Inspired by ariadne and
    https://tomaugspurger.net/posts/serializing-dataclasses/

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -5,6 +5,7 @@ import base64
 import datetime as dt
 from contextlib import contextmanager
 from dataclasses import dataclass
+from decimal import Decimal
 from typing import Callable, Generator
 from unittest import TestCase, mock
 
@@ -326,6 +327,31 @@ class SerializableTests(TestCase):
 
     def test_already_serializable(self) -> None:
         self.assertEqual("hello world", "hello world")
+
+
+class DataclassConversionTests(TestCase):
+    """Tests both decode_to and convert_to"""
+
+    def test(self) -> None:
+        @dataclass
+        class MyDataclass:
+            name: str
+            balance: Decimal
+            due: dt.date
+
+        @utils.convert_to(MyDataclass, "balance")
+        def _(value: str) -> Decimal:
+            return Decimal(value)
+
+        @utils.convert_to(MyDataclass, "due")
+        def _(value: str) -> dt.date:
+            return dt.date.fromisoformat(value)
+
+        data = {"name": "marduk", "balance": "5.00", "due": "2025-02-16"}
+        result = utils.decode_to(MyDataclass, data)
+
+        expected = MyDataclass("marduk", Decimal("5.00"), dt.date(2025, 2, 16))
+        self.assertEqual(expected, result)
 
 
 @contextmanager
