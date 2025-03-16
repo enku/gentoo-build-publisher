3c6d3bec0ba06c270fec3d89748f171580238355
Albert Hopkins <marduk@letterboxes.org>
2025-03-15 19:06:59 -0500

    ui: add the plugins link to the navbar
    Add a link to the navbar which goes to the new plugins page.

--- src/gentoo_build_publisher/views/__init__.py
+++ src/gentoo_build_publisher/views/__init__.py
@@ -46,7 +46,7 @@ def _(request: HttpRequest, machine: str) -> ViewContext:
     return ctx.create_machine_context(input_context)
 
 
-@view("plugins/")
+@view("plugins/", name="gbp-plugins")
 @render("gentoo_build_publisher/plugins/main.html")
 def _(request: HttpRequest) -> ViewContext:
     return ctx.create_plugins_context()
