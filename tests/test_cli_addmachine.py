f5e605e896b68ede5aaf1459c68d08a9055b294d
Albert Hopkins <marduk@letterboxes.org>
2025-07-03 08:10:35 -0500

    tests: enhance a test

--- tests/test_cli_addmachine.py
+++ tests/test_cli_addmachine.py
@@ -2,6 +2,7 @@
 
 # pylint: disable=missing-docstring
 from argparse import ArgumentParser, Namespace
+from unittest import mock
 
 from unittest_fixtures import Fixtures, given
 
@@ -13,17 +14,39 @@ from gentoo_build_publisher.types import MachineJob, Repo
 
 @given("gbp", "console")
 class AddMachineTestCase(TestCase):
-    def test_calls_graphql_with_the_expected_args(self, fixtures: Fixtures) -> None:
+    def test_calls_jenkins_with_the_expected_args(self, fixtures: Fixtures) -> None:
+        # Given the command-line args
         args = Namespace(
             name="base",
             repo="https://github.com/enku/gbp-machines.git",
             branch="master",
             deps=["gentoo"],
         )
-        console = fixtures.console
-        exit_status = addmachine.handler(args, fixtures.gbp, console)
 
+        # When we call the addmachine handler
+        exit_status = addmachine.handler(args, fixtures.gbp, fixtures.console)
+
+        # Then it calls Jenkins with the expected args
         self.assertEqual(exit_status, 0)
+        jenkins = publisher.jenkins
+        jenkins.session.post.assert_called_with(
+            "https://jenkins.invalid/createItem",
+            data=mock.ANY,
+            headers={"Content-Type": "text/xml"},
+            params={"name": "base"},
+        )
+        data = jenkins.session.post.call_args[1]["data"]
+        self.assertIn(
+            "<url>https://github.com/enku/gbp-machines.git</url>",
+            data,
+            "machine repo not found in post data",
+        )
+        self.assertIn("<name>*/master</name>", data, "branch not found in post data")
+        self.assertIn(
+            "<upstreamProjects>repos/gentoo</upstreamProjects>",
+            data,
+            "ebuild repo not found in post data",
+        )
 
     def test_when_item_already_exists(self, fixtures: Fixtures) -> None:
         job = MachineJob(
