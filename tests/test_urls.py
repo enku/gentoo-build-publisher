580698ef6d55ba240c18721caf2e2fe3cada30f9
Albert Hopkins <marduk@letterboxes.org>
2025-08-31 06:58:11 -0500

    tests: add tests for urls.plugin_urlpatterns
    Add some needed tests for this helper function.

---
+++ tests/test_urls.py
@@ -0,0 +1,31 @@
+"""Tests for gentoo_build_publisher.urls"""
+
+# pylint: disable=missing-docstring
+from unittest import TestCase
+
+from gentoo_build_publisher import plugins, urls
+from gentoo_build_publisher.django.gentoo_build_publisher.urls import urlpatterns
+
+
+class PluginURLPatterns(TestCase):
+    def test(self) -> None:
+        plugin = plugins.Plugin(
+            name="test",
+            app="test",
+            version="1.0",
+            description="A test app",
+            graphql=None,
+            urls="gentoo_build_publisher.django.gentoo_build_publisher.urls",
+        )
+        self.assertEqual(urls.plugin_urlpatterns(plugin), urlpatterns)
+
+    def test_none(self) -> None:
+        plugin = plugins.Plugin(
+            name="test",
+            app="test",
+            version="1.0",
+            description="A test app",
+            graphql=None,
+            urls=None,
+        )
+        self.assertEqual(urls.plugin_urlpatterns(plugin), [])
