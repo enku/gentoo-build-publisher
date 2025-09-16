b2c1d976b4fa792008d3a868c087f2d5f338b2a7
Albert Hopkins <marduk@letterboxes.org>
2025-09-16 15:09:16 -0500

    signals.PyDispatcherAdapter: allow a handler to handler multiple events
    Allow a single event handler to handle multiple events. This is a fix to
    a bug found in gbp-fl[1] where subsequend calls to `.bind()` using the
    same handler would allow the previously bound signals to get garbage
    collected.
    
    Also tests were not included when `PyDispatcherAdapter` was introduced
    so this change adds needed tests.
    
    [1] https://github.com/enku/gbp-fl/commit/565c0ae1d69572814eb145d21929a2e174bdb62e

--- src/gentoo_build_publisher/signals.py
+++ src/gentoo_build_publisher/signals.py
@@ -31,12 +31,22 @@ class EventExistsError(Exception):
         return f'"{self.name}" already exists'
 
 
+class NotBoundError(Exception):
+    """Unbinding an event that was not bound to an event"""
+
+    def __init__(self, handler: PyDispatchHandler):
+        self.handler = handler
+
+    def __str__(self) -> str:
+        return f'"{self.handler.__name__}" is not bound to an event'
+
+
 class PyDispatcherAdapter:
     """Adapter to run python-dispatcher handlers through blinker"""
 
     def __init__(self) -> None:
         self._registry: dict[str, Signal] = {}
-        self._handlers: dict[PyDispatchHandler, BlinkerHandler[Any]] = {}
+        self._handlers: dict[PyDispatchHandler, set[BlinkerHandler[Any]]] = {}
 
         event: str
         for event in getattr(self, "_events_", []):
@@ -58,8 +68,9 @@ class PyDispatcherAdapter:
             except KeyError as error:
                 raise DoesNotExistError(signal_name) from error
 
-            self._handlers[callback] = blinker_adapter(callback)
-            sig.connect(self._handlers[callback])
+            adapted_callback = blinker_adapter(callback)
+            self._handlers.setdefault(callback, set()).add(adapted_callback)
+            sig.connect(adapted_callback)
 
     def emit(self, signal_name: str, *args: Any, **kwargs: Any) -> None:
         """Emit the given signal"""
@@ -73,7 +84,10 @@ class PyDispatcherAdapter:
     def unbind(self, callback: PyDispatchHandler) -> None:
         """Unbind the given signal handler"""
         # remove the reference
-        self._handlers.pop(callback)
+        try:
+            self._handlers.pop(callback)
+        except KeyError as error:
+            raise NotBoundError(callback) from error
 
     def get_dispatcher_event(self, name: str) -> Signal:
         """Return the given registered event (Signal)
--- tests/test_signals.py
+++ tests/test_signals.py
@@ -1,6 +1,7 @@
 """Tests for Gentoo Build Publisher signals"""
 
 # pylint: disable=missing-docstring
+from typing import Any
 from unittest import TestCase
 
 from unittest_fixtures import Fixtures, params
@@ -35,3 +36,147 @@ class RegisterTests(TestCase):
 
         with self.assertRaises(signals.EventExistsError):
             dispatcher.register_event("test_event")
+
+
+class PyDispatcherAdapterTests(TestCase):
+    # pylint: disable=protected-access
+    def test_register_event(self) -> None:
+        d = signals.PublisherDispatcher()
+
+        d.register_event("test_event1", "test_event2")
+
+        self.assertIn("test_event1", d._registry)
+        self.assertIn("test_event2", d._registry)
+
+    def test_register_same_event(self) -> None:
+        d = signals.PublisherDispatcher()
+
+        d.register_event("test_event")
+
+        with self.assertRaises(signals.EventExistsError):
+            d.register_event("test_event")
+
+    def test_built_in_signals(self) -> None:
+        class MyDispatcher(signals.PublisherDispatcher):
+            _events_ = ["this", "that", "the", "other"]
+
+        d = MyDispatcher()
+
+        for event in MyDispatcher._events_:
+            self.assertIn(event, d._registry)
+
+    def test_bind_and_emit(self) -> None:
+        called = False
+        d = signals.PublisherDispatcher()
+        d.register_event("test_event")
+
+        def handler(**kwargs: Any) -> None:
+            nonlocal called
+            self.assertEqual(kwargs, {"this": "that", "the": "other"})
+            called = True
+
+        d.bind(test_event=handler)
+        d.emit("test_event", this="that", the="other")
+
+        self.assertTrue(called)
+
+    def test_same_handler_multiple_events(self) -> None:
+        handled_event1 = False
+        handled_event2 = False
+        d = signals.PublisherDispatcher()
+
+        d.register_event("event1", "event2")
+
+        def handler(event: str) -> None:
+            nonlocal handled_event1, handled_event2
+
+            if event == "event1":
+                handled_event1 = True
+            if event == "event2":
+                handled_event2 = True
+
+        d.bind(event1=handler)
+        d.bind(event2=handler)
+
+        d.emit("event1", event="event1")
+        self.assertTrue(handled_event1)
+        self.assertFalse(handled_event2)
+
+        handled_event1 = False
+        d.emit("event2", event="event2")
+        self.assertFalse(handled_event1)
+        self.assertTrue(handled_event2)
+
+    def test_unbind(self) -> None:
+        called = False
+        d = signals.PublisherDispatcher()
+        d.register_event("test_event")
+
+        def handler(**kwargs: Any) -> None:
+            nonlocal called
+            self.assertEqual(kwargs, {"this": "that", "the": "other"})
+            called = True
+
+        d.bind(test_event=handler)
+        d.emit("test_event", this="that", the="other")
+        self.assertTrue(called)
+
+        called = False
+        d.unbind(handler)
+        d.emit("test_event", this="that", the="other")
+        self.assertFalse(called)
+
+    def test_unbind_same_handler_multiple_events(self) -> None:
+        handled_event1 = False
+        handled_event2 = False
+        d = signals.PublisherDispatcher()
+
+        d.register_event("event1", "event2")
+
+        def handler(event: str) -> None:
+            nonlocal handled_event1, handled_event2
+
+            if event == "event1":
+                handled_event1 = True
+            if event == "event2":
+                handled_event2 = True
+
+        d.bind(event1=handler)
+        d.bind(event2=handler)
+
+        d.emit("event1", event="event1")
+        d.emit("event2", event="event2")
+        self.assertTrue(handled_event1)
+        self.assertTrue(handled_event2)
+
+        d.unbind(handler)
+        handled_event1 = False
+        handled_event2 = False
+
+        d.emit("event1", event="event1")
+        d.emit("event2", event="event2")
+        self.assertFalse(handled_event1)
+        self.assertFalse(handled_event2)
+
+    def test_unbind_not_registered(self) -> None:
+        d = signals.PublisherDispatcher()
+
+        def handler(**kwargs: Any) -> None:
+            pass
+
+        with self.assertRaises(signals.NotBoundError):
+            d.unbind(handler)
+
+    def test_get_dispatcher_event(self) -> None:
+        d = signals.PublisherDispatcher()
+        d.register_event("test_event")
+
+        signal = d.get_dispatcher_event("test_event")
+
+        self.assertEqual(signal.name, "test_event")  # type: ignore[attr-defined]
+
+    def test_get_dispatcher_event_with_unregistered_event(self) -> None:
+        d = signals.PublisherDispatcher()
+
+        with self.assertRaises(signals.DoesNotExistError):
+            d.get_dispatcher_event("bogus")
