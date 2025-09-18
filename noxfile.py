7dad211100ac4da927b60b1205e520339e5a57ac
Albert Hopkins <marduk@letterboxes.org>
2025-09-18 11:29:03 -0500

    drop support for Python 3.11
    Gentoo Build Publisher will now require Python 3.12+

--- noxfile.py
+++ noxfile.py
@@ -2,7 +2,7 @@
 import nox
 
 
-@nox.session(python=("3.11", "3.12", "3.13", "3.14"))
+@nox.session(python=("3.12", "3.13", "3.14"))
 def tests(session: nox.Session) -> None:
     session.run("pdm", "install", "--dev", "-G:all", external=True)
     session.run("pdm", "run", "coverage", "run", "-m", "tests", external=True)
