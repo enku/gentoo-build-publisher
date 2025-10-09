0fcb0971940e23943f3af2b710d2d38684d7def0
Albert Hopkins <marduk@letterboxes.org>
2025-10-09 13:52:11 -0500

    noxfile: install everything in one call
    Don't call .install() twice when everything can be installed in one
    call.

--- noxfile.py
+++ noxfile.py
@@ -4,11 +4,10 @@ import nox
 
 @nox.session(python=("3.12", "3.13", "3.14"))
 def tests(session: nox.Session) -> None:
-    session.install(".[redis,test]")
     dev_dependencies = nox.project.load_toml("pyproject.toml")["dependency-groups"][
         "dev"
     ]
-    session.install(*dev_dependencies)
+    session.install(".[redis,test]", *dev_dependencies)
 
     session.run("coverage", "run", "-m", "tests")
     session.run("coverage", "report", "-m")
