999819ed86892c5bbd7965ffd68a87a24120d537
Albert Hopkins <marduk@letterboxes.org>
2026-05-30 19:19:34 -0500

    tests: linting

--- tests/test_templates.py
+++ tests/test_templates.py
@@ -8,15 +8,13 @@ from django.template import Context, Template
 
 class BaseExtraHeadTests(TestCase):
     def test(self) -> None:
-        template = Template(
-            """
+        template = Template("""
             {% extends "gentoo_build_publisher/base.html" %}
             {% block extra_head %}
             {{ block.super }}
             This is a test
             {% endblock %}
-            """
-        )
+            """)
         content = template.render(Context({}))
 
         self.assertIn("This is a test", content)
